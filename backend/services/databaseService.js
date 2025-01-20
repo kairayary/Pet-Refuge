f4f1b987e028c8c200a21b9904ebecdcbbda8e44
kairayary <kaira.abreu@gmail.com>
2025-01-19 21:12:21 -0300

    Creación de las funciones para leer y guardar datos del archivo JSON

---
+++ backend/services/databaseService.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require ('path');
+
+const dbPath = path.join(__dirname, '../data/database.json');
+
+//Para leer los datos del archivo JSON
+
+function readDatabase() {
+    const data = fs.readFileSync(dbPath, 'utf8');
+    return JSON.parse(data)    
+};
+
+
+//Guardar los datos del archivo JSON
+
+function writeDatabase(data) {
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8')
+};
+
+module.exports = {readDatabase, writeDatabase};
