2646189936f9162d10e5df149991ba07ef3d5ddb
kairayary <kaira.abreu@gmail.com>
2025-01-20 20:53:01 -0300

    se añadio el middLeware

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {registerUser, loginUser}= require('../controllers/usersController');
+const {authenticateToken} = require('../middLeware/authMiddleware');
 const router = express.Router();
 
 //Registra un nuevo usuario
@@ -7,5 +8,11 @@ router.post("/register", registerUser);
 
 //Inicio de sesión de usuario existente
 router.post("/login", loginUser);
+
+//Ruta protegida que solo usuarios autenticados puedan acceder
+
+router.get("/profile",authenticateToken, (req,res)=>{
+    res.json({message:"Perfil de usuario", user:req.user});
+})
 
 module.exports = router;
