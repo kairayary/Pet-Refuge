0ea745c6591c03affc05d5a2a6fea138a5178306
kairayary <kaira.abreu@gmail.com>
2025-01-20 20:43:02 -0300

    se agrego el archivo authMiddleware con la función que verifica si el usuario esta autenticado antes de acceder a las rutas protegidas

---
+++ backend/middLeware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+//MiddLeware para autenticar el token JWT
+function authenticateToken(req,res,next) {
+
+    //Para obtener el token de los encabezados de la solicitud
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    //Verifica si no se proporciono el token
+    if (!token) {
+        return res.status(401).json({error:'Acceso denegao. No se proporcionó token '});   
+    };
+
+    //Para verificar el token con la clave secreta
+    jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
+        if (err) {
+            return res.status(403).json({error:'Token inválido o expirado'});
+
+        };
+        //Agrega el usuario al object req para que este disponible en las rutas
+        req.user = user;
+        
+        //Es para continuar con el proximo middleware o ruta
+        next();
+    });
+    
+};
+
+module.exports = {authenticateToken};
