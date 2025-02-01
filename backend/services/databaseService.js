e31764a2125bb97e0b463e4c6fb06bfdbacca4d8
kairayary <kaira.abreu@gmail.com>
2025-01-31 21:50:09 -0300

    Se agregó la función savePetsToDatabase

--- backend/services/databaseService.js
+++ backend/services/databaseService.js
@@ -5,17 +5,22 @@ const dbPath = path.join(__dirname, '../data/database.json');
 
 function readDatabase() {
     try {
-        if (!fs.existsSync(dbPath)) {
-            // Si el archivo no existe, lo crea con una estructura vacía
-            fs.writeFileSync(dbPath, JSON.stringify({ pets: [], users: [], adoptions: [] }, null, 2), 'utf8');
-        }
+        
         const data = fs.readFileSync(dbPath, 'utf8');
-        return JSON.parse(data);
+        const db = JSON.parse(data);
+
+        // Asegurar que existan todos los array
+        if (!db.users) db.users = [];
+        if (!db.pets) db.pets = [];
+        if (!db.adoptions) db.adoptions = [];
+
+        return db;
     } catch (error) {
-        console.error("Error al leer la base de datos: ", error);
-        return { pets: [], users: [], adoptions: [] };
+        console.error("Error al leer la base de datos:", error);
+        console.error("Ruta de la base de datos:", dbPath); // Para verificar si la ruta es correcta
+        return { users: [], pets: [], adoptions: [] };
     }
-}
+};
 
 function writeDatabase(data) {
     try {
@@ -23,6 +28,28 @@ function writeDatabase(data) {
     } catch (error) {
         console.error("Error al guardar la base de datos: ", error);
     }
-}
+};
 
-module.exports = { readDatabase, writeDatabase };
+// Obtener las mascotas de la base de datos
+function getPetsFromDatabase() {
+    const data = readDatabase();  // Llama a readDatabase para obtener los datos
+    return data.pets;  // Retorna el array de mascotas
+};
+
+function savePetsToDatabase(pets, adoptions) {
+    
+    try {
+        // Primero se  carga los datos actuales
+        const data = readDatabase();
+
+        // Luego se actualiza los arrays de mascotas y adopciones
+        data.pets = pets;
+        data.adoptions = adoptions;
+
+        //Se guarda los datos actualizados
+        fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf8');
+    } catch (error) {
+        console.error("Error al guardar la base de datos: ", error);
+    }
+}
+module.exports = { readDatabase, writeDatabase,getPetsFromDatabase, savePetsToDatabase };
